911cbcd2bda056eb7685c8327d50f84217dbeb00
root <nafmuriu@gmail.com>
2019-03-11 17:47:34 +0300

    12 commit

---
+++ sidebar.php
@@ -0,0 +1,10 @@
+<div style="height:220px;background:#1e90ff;text-align:center;color:white">
+<center><img src="img/naf1.jpeg" class="img img-circle" style="height:180px;width:180px;text-align:center;"/></center>
+<h4>Administrator <?php echo $user; ?></h4>
+</div>
+<ul class="list-group-menu list-group"></ul>
+<li class="list-group-item"><a href="success_admin.php?add"><i class="fa fa-plus"></i>&nbsp;&nbsp;Add Users</a></li>
+<li class="list-group-item"><a href="success_admin.php?activate"><i class="fa fa-refresh"></i>&nbsp;&nbsp;Activate Users</a></li>
+<li class="list-group-item"><a href="success_admin.php?delete"><i class="fa fa-trash"></i>&nbsp;&nbsp;Delete Accounts</a></li>
+<!--<li class="list-group-item"><a href="#"><i class="fa fa-hdd-o"></i>&nbsp;&nbsp;Back Up/Restore</a></li>
+<li class="list-group-item"><a href="#"><i class="fa fa-gears"></i>&nbsp;&nbsp;Settings</a></li><div style="height:220px;background:#1e90ff;text-align:center;color:white">-->
