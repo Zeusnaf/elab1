a7b78061abc085a6fd300ee54c821a713e4d9dc0
root <nafmuriu@gmail.com>
2019-03-14 15:23:35 +0300

    13 commit

---
+++ sidebar_lab.php
@@ -0,0 +1,12 @@
+<div style="height:220px;background:#1e90ff;text-align:center;color:white">
+<center><img src="img/lab7.jpg" class="" style="height:180px;width:285px;text-align:center;"/></center>
+<h4>Lab Technician <?php echo $user; ?></h4>
+</div>
+<ul class="list-group-menu list-group"></ul>
+
+<li class="list-group-item"><a href="success_lab.php?view"><i class="fa fa-eye"></i>&nbsp;&nbsp;View Equipments</a></li>
+<li class="list-group-item"><a href="success_lab.php?borrowed"><i class="fa fa-check"></i>&nbsp;&nbsp;Borrowed Items</a></li>
+<li class="list-group-item"><a href="success_lab.php?broken"><i class="fa fa-chain-broken"></i>&nbsp;&nbsp;Broken</a></li>
+
+
+<li class="list-group-item"><a href="success_lab.php?add"><i class="fa fa-edit"></i>&nbsp;&nbsp;Update Used chemicals</a></li>
