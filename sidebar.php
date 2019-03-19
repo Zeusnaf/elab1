1138e77e22efb9f1f143b7d66bbcd12e5abd929b
root <nafmuriu@gmail.com>
2019-03-19 22:46:47 +0300

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,5 +6,5 @@
 <li class="list-group-item"><a href="success_admin.php?add"><i class="fa fa-plus"></i>&nbsp;&nbsp;Add Users</a></li>
 <li class="list-group-item"><a href="success_admin.php?activate"><i class="fa fa-refresh"></i>&nbsp;&nbsp;Activate Users</a></li>
 <li class="list-group-item"><a href="success_admin.php?delete"><i class="fa fa-trash"></i>&nbsp;&nbsp;Delete Accounts</a></li>
-<!--<li class="list-group-item"><a href="#"><i class="fa fa-hdd-o"></i>&nbsp;&nbsp;Back Up/Restore</a></li>
-<li class="list-group-item"><a href="#"><i class="fa fa-gears"></i>&nbsp;&nbsp;Settings</a></li><div style="height:220px;background:#1e90ff;text-align:center;color:white">-->
+<li class="list-group-item"><a href="#"><i class="fa fa-hdd-o"></i>&nbsp;&nbsp;Back Up/Restore</a></li>
+<li class="list-group-item"><a href="#"><i class="fa fa-gears"></i>&nbsp;&nbsp;Settings</a></li><div style="height:220px;background:#1e90ff;text-align:center;color:white">
