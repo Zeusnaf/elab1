6dfe1ae30f7bc58cdc3edfd1e5b134fb0acd2019
root <nafmuriu@gmail.com>
2019-03-28 18:11:09 +0300

    sidebar-sec

---
+++ sidebar_sec.php
@@ -0,0 +1,8 @@
+<div style="height:220px;background:#1e90ff;text-align:center;color:white">
+<center><img src="img/lab1.jpg" class="" style="height:180px;width:285px;text-align:center;"/></center>
+<h4>Secretary <?php echo $user; ?></h4>
+</div>
+<ul class="list-group-menu list-group"></ul>
+<li class="list-group-item"><a href="success_sec.php?add"><i class="fa fa-plus"></i>&nbsp;&nbsp;Add Lab Equipments</a></li>
+<li class="list-group-item"><a href="success_sec.php?view"><i class="fa fa-eye"></i>&nbsp;&nbsp;View Equipments</a></li>
+
